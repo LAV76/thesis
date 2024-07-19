1ba229a5c4ec16b2dd71f0e80e4fe41f97f98442
Alexander Lekontsev <5958383@mail.ru>
2024-07-19 10:28:50 +0300

    added indicator functions

--- code/functions.py
+++ code/functions.py
@@ -145,3 +145,106 @@ def get_symbol_price(symbol):
     prices = client.get_all_tickers()
     df=pd.DataFrame(prices)
     return float(df[ df['symbol']==symbol]['price'].iloc[0])
+
+
+# Индикаторы
+
+# поиск линии цены
+def indSlope(series,n):
+    
+    array_sl = [j*0 for j in range(n-1)]
+    
+    for j in range(n,len(series)+1):
+        y = series[j-n:j]
+        x = np.array(range(n))
+        x_sc = (x - x.min())/(x.max() - x.min())
+        y_sc = (y - y.min())/(y.max() - y.min())
+        x_sc = sm.add_constant(x_sc)
+        model = sm.OLS(y_sc,x_sc)
+        results = model.fit()
+        array_sl.append(results.params.iloc[-1])
+    slope_angle = (np.rad2deg(np.arctan(np.array(array_sl))))
+    return np.array(slope_angle)
+
+
+def indATR(source_DF,n):
+    df = source_DF.copy()
+    df['H-L']=abs(df['high']-df['low'])
+    df['H-PC']=abs(df['high']-df['close'].shift(1))
+    df['L-PC']=abs(df['low']-df['close'].shift(1))
+    df['TR']=df[['H-L','H-PC','L-PC']].max(axis=1,skipna=False)
+    df['ATR'] = df['TR'].rolling(n).mean()
+    df_temp = df.drop(['H-L','H-PC','L-PC'],axis=1)
+    return df_temp
+
+
+# ишем локальный min / max
+def isLCC(DF,i):
+    df=DF.copy()
+    LCC=0
+    
+    if df['close'][i]<=df['close'][i+1] and df['close'][i]<=df['close'][i-1] and df['close'][i+1]>df['close'][i-1]:
+        #найдено дно
+        LCC = i-1;
+    return LCC
+
+def isHCC(DF,i):
+    df=DF.copy()
+    HCC=0
+    if df['close'][i]>=df['close'][i+1] and df['close'][i]>=df['close'][i-1] and df['close'][i+1]<df['close'][i-1]:
+        #найдена вершина
+        HCC = i;
+    return HCC
+
+
+# получить ценовой канал
+def getMaxMinChannel(DF, n):
+    maxx=0
+    minn=DF['low'].max()
+    for i in range (1,n):
+        if maxx<DF['high'][len(DF)-i]:
+            maxx=DF['high'][len(DF)-i]
+        if minn>DF['low'][len(DF)-i]:
+            minn=DF['low'][len(DF)-i]
+    return(maxx,minn)
+
+
+# Добавляем новые данные
+def PrepareDF(DF):
+    ohlc = DF.iloc[:,[0,1,2,3,4,5]]
+    ohlc.columns = ["date","open","high","low","close","volume"]
+    ohlc=ohlc.set_index('date')
+    df = indATR(ohlc,14).reset_index()
+    df['slope'] = indSlope(df['close'],5)
+    df['channel_max'] = df['high'].rolling(10).max()
+    df['channel_min'] = df['low'].rolling(10).min()
+    df['position_in_channel'] = (df['close']-df['channel_min']) / (df['channel_max']-df['channel_min'])
+    df = df.set_index('date')
+    df = df.reset_index()
+    return(df)
+
+
+def check_if_signal(symbol):
+    ohlc = get_futures_klines(symbol,100)
+    prepared_df = PrepareDF(ohlc)
+    signal=""
+    
+    i=98
+    
+    if isLCC(prepared_df,i-1)>0:
+       # найдкно дно - OPEN LONG
+        if prepared_df['position_in_channel'][i-1]<0.5:
+            # близко к вершине канала
+            if prepared_df['slope'][i-1]<-20:
+                # точка входа в LONG
+                signal='long'
+
+    if isHCC(prepared_df,i-1)>0:
+       # найден максимум - OPEN SHORT
+        if prepared_df['position_in_channel'][i-1]>0.5:
+            # близко к вершине канала
+            if prepared_df['slope'][i-1]>20:
+                # точка входа в SHORT
+                signal='short'
+
+    return signal
